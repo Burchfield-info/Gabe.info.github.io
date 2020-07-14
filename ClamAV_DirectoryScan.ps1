8a65ba867bda42ab0e18c129749a2f16ecc57599
ROT-userhandle <39745193+ROT-userhandle@users.noreply.github.com>
2020-07-14 18:31:12 -0400

    Create ClamAV_DirectoryScan.ps1
    Basic PowerShell "Scriptlet" that allows for the quick scan of any given directory with Clam AV. This Script assumes that you have installed ClamAV on your C: Drive in Program Files, otherwise it will need negligible modification. Line six, as of this commit, (cd "C:\Program Files\ClamAV")
    
    Furthermore this script will prompt the user for;
    
    Directory to scan
    Directory to store quarantined files
    Directory to store the log file(automatically appends the log)
    
    Lastly, the script will print out the log and stay alive for 2 minutes, another negligible modification.
    
    
    In order to run this script you may need to changer the file permissions for the downloaded file, as well as Unblock this script via PowerShell which will obviously need to be run as Administrator. I am in now way responsible for the loss of any files, and furthermore you'll likely need to run the command below, note, it assumes you've put the file on your desktop and that your username is username... Be a good sport change the Path to reflect where ever you have downloaded this file to and ensure to include the filename.
    
    > Unblock-File -Path C:\Users\Username\Desktop\ClamAV_DirectoryScan.ps1.ps1

---
+++ ClamAV_DirectoryScan.ps1
@@ -0,0 +1,15 @@
+$Dir_to_scan = Read-Host "Enter the directory/file you wish to scan. Example c:\folder\filename.txt"
+$User_quar_Dir = Read-Host "Choose a Directory for storing Quarantined files"
+Write-Warning -Message "Must write-out the filename as well. Example c:\folder\filename.txt"
+$Log_dir = Read-Host "Choose a Directory for storing ClamAV Logs"
+
+<#Simply input the file path for ClamAV inside the parenthesis "insert-filepath" #>
+cd "C:\Program Files\ClamAV"
+Write-Host "Scanning started. Grab some Coffee"
+./clamscan -r $Dir_to_scan --move=$User_quar_Dir --log=$Log_dir
+Write-Host "Scan Complete!" -ForegroundColor white -BackgroundColor red
+type $Log_dir
+Write-Host "Automatically closes after 2 Minutes" -ForegroundColor white -BackgroundColor red
+
+<#Simply Adjust the value 120 hear as it represents seconds which is clearly stated.#>
+Start-Sleep -Seconds 120
